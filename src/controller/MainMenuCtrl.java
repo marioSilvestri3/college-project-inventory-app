2b7f40e3a8a21b4991d27c2d1a2d9ccee48a29c3
mariosilvestri3 <29622032+mariosilvestri3@users.noreply.github.com>
2020-07-13 17:36:24 -0500

    7/13 update

---
+++ src/controller/MainMenuCtrl.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenuCtrl {
+
+    @FXML
+    private Button main_exit;
+
+    @FXML
+    void exit_button(ActionEvent event) {
+
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/main_menu.fxml"));
+        primaryStage.setTitle("Mario Silvestri - C482 - Inventory");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
