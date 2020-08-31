abb9358887ae0f2c3230a57d2e61c2dcb1763a76
mariosilvestri3 <29622032+mariosilvestri3@users.noreply.github.com>
2020-08-31 11:58:22 -0500

    Update to Java 11; Add Exit Button Functionality

--- src/controller/MainMenuCtrl.java
+++ src/controller/MainMenuCtrl.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainMenuCtrl {
 
@@ -10,10 +11,57 @@ public class MainMenuCtrl {
     private Button main_exit;
 
     @FXML
-    void exit_button(ActionEvent event) {
+    public void main_exit(ActionEvent event) {
+        Stage stage = (Stage) main_exit.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private Button main_add_part;
+
+    @FXML
+    private Button main_mod_part;
+
+    @FXML
+    private Button main_delete_part;
+
+    @FXML
+    private Button main_add_product;
+
+    @FXML
+    private Button main_modify_product;
+
+    @FXML
+    private Button main_delete_product;
+
+    @FXML
+    void main_add_part(ActionEvent event) {
 
     }
 
-    public void exit_application(ActionEvent actionEvent) { //TODO
+    @FXML
+    void main_add_product(ActionEvent event) {
+
     }
+
+    @FXML
+    void main_delete_part(ActionEvent event) {
+
+    }
+
+    @FXML
+    void main_delete_product(ActionEvent event) {
+
+    }
+
+    @FXML
+    void main_modify_part(ActionEvent event) {
+
+    }
+
+    @FXML
+    void main_modify_product(ActionEvent event) {
+
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/** Creates a desktop application that manages part and product inventory */
 public class Main extends Application {
 
     @Override
@@ -18,6 +19,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
